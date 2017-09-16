8e2ed0338a1661d7d37746c79c7db95e8933a689
ludovicm67 <ludovicmuller1@gmail.com>
2017-09-17 00:25:58 +0200

    added some tests

---
+++ tests/RequestTest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace ludovicm67\Request\Tests;
+
+use PHPUnit\Framework\TestCase;
+use ludovicm67\Request\Exception\RequestException;
+use ludovicm67\Request\Request;
+use ludovicm67\Request\RequestBuilder;
+
+class RequestTest extends TestCase {
+
+  public function testUrlCheck() {
+    $builder = new RequestBuilder('bad-url');
+    $this->expectException(RequestException::class);
+    $request = new Request($builder);
+  }
+
+  public function testRequest() {
+    $builder = new RequestBuilder('https://github.com/ludovicm67/php-request');
+    $request = new Request($builder);
+    $this->assertFalse($request->isEmpty());
+  }
+
+}
